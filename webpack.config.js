82c2711d366dfd9da0bc1d03b83e5593f21cc6d7
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-10-23 21:19:35 +1300

    removed webpack config and added tsconfig

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // other configurations...
-    externals: {
-      '@vencord/types/utils/types': 'commonjs @vencord/types/utils/types',
-    },
-    // more configurations...
-  };
