ab24e485287e41f97fb74561bb93e1de8986bb31
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-10-23 21:56:25 +1300

    Added global context menu debugging

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 // import definePlugin from "@utils/types";
 //import { definePluginSettings } from "@vencord/types/api/settings";
 import definePlugin, { OptionType } from "@utils/types";
+import { GlobalContextMenuPatchCallback, removeContextMenuPatch } from "@utils/types/api/ContextMenu";
 
+// const myGlobalContextMenuPatch: removeContextMenuPatch = (navId, children, ...args) => {
+//     // You can log navId, args, or manipulate children here
+//     console.log('Nav ID:', navId);
+//     console.log('Arguments:', args);
+
+//     // Modify or enhance the context menu items
+//     const modifiedChildren = [...children];
+// }
+const patchContextMenu: GlobalContextMenuPatchCallback = (navId, children) => () => {
+    console.log(navId)
+};
 export default definePlugin({
     name: "hideUser",
     description: "This hides a user like friend from DM's and friends page",
