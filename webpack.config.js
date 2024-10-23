6fb581274e5c4297a66f69b8e419c8a93b1a0456
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2024-10-23 21:07:38 +1300

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // other configurations...
+    externals: {
+      '@vencord/types/utils/types': 'commonjs @vencord/types/utils/types',
+    },
+    // more configurations...
+  };
